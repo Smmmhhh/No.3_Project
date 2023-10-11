9cb899e38a6514f87f62eb91fc08e79ea8c1f13c
Smmmhhh <kua0408@gmail.com>
2023-10-11 12:12:29 +0900

    [UPDATE] ControllerTest

--- src/main/java/com/treemarket/tree/TreeApplication.java
+++ src/main/java/com/treemarket/tree/TreeApplication.java
@@ -1,7 +1,14 @@
 package com.treemarket.tree;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class TreeApplication {
@@ -10,4 +17,17 @@ public class TreeApplication {
 		SpringApplication.run(TreeApplication.class, args);
 	}
 
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
+		//.xml 형태의 모든 파일을 받기때문에 배열로 받는다.
+		// 만약 한가지만 받으면 배열로[] 받을 필요가 없다.
 }
