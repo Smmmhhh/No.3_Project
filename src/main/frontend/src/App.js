28139148eb01521bb73cb9f9c0bc12a7d93ee257
yoonhyungwoo <guddn13212@naver.com>
2023-10-12 09:35:54 +0900

    ADD : main페이지 구성

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Login";
+import Register from "./Register";
+import Product from "./Product";
+import MyMain from "./MyMain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          안녕하시렵니까
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyMain />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
