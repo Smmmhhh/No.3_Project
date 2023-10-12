ebd23fd9a399ca11b708b1987472d11457bf9b5e
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-10-12 17:21:50 +0900

    [UPDATE] addressParse

--- src/main/java/com/treemarket/tree/service/AddressService.java
+++ src/main/java/com/treemarket/tree/service/AddressService.java
@@ -3,5 +3,5 @@ package com.treemarket.tree.service;
 import com.treemarket.tree.domain.AddressVO;
 
 public interface AddressService {
-    Long getAddressId(AddressVO addressVO);
+    Long getAddressId(String inputAddress);
 }
