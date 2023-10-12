2335a24419934c6b5479f4a4fa1036f5a4107658
yoonhyungwoo <guddn13212@naver.com>
2023-10-12 18:27:58 +0900

    ADD : MyPage 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,6 +4,8 @@ import Login from "./Login/Login";
 import Register from "./Register/Register";
 import Product from "./Product";
 import MyMain from "./MyMain";
+import UsersEdit from "./UsersEdit";
+import MyPage from "./MyPage/MyPage";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/users" element={<UsersEdit />} />
       </Routes>
     </BrowserRouter>
   );
