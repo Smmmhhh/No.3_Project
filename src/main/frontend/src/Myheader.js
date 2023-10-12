d15b688a1c78332d40c6d750b803578902f3e2e8
yoonhyungwoo <guddn13212@naver.com>
2023-10-12 12:11:18 +0900

    ADD : Images

--- src/main/frontend/src/Myheader.js
+++ src/main/frontend/src/Myheader.js
@@ -6,7 +6,7 @@ const MyHeader = () => {
     <header>
       <div className="header_left">
         <Link to="/" style={{ width: "200px" }}>
-          <img className="logo_img" src="/image/logo.png" />
+          <img className="logo_img" src="/assets/logo.png" />
         </Link>
         <Link to="/product">
           <h1 className="category">중고 거래</h1>
@@ -14,7 +14,7 @@ const MyHeader = () => {
       </div>
       <div className="search">
         <input type="text" />
-        <img src="/image/search_img.png" />
+        <img src="/assets/search_img.png" />
       </div>
       <div className="header_right">
         <Link to="/login">
