6166f97e7da1e470ecc859fe4f9363fbf61f3b1e
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-10-18 14:40:04 +0900

    [UPDATE] Chatroom Mapper

--- src/main/java/com/treemarket/tree/dto/Chatroom/ChatroomRequest.java
+++ src/main/java/com/treemarket/tree/dto/Chatroom/ChatroomRequest.java
@@ -1,4 +1,14 @@
 package com.treemarket.tree.dto.Chatroom;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatroomRequest {
+    private Long postId;
+    private Long buyerNo;
+
 }
