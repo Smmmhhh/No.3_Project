bf684998a90966e1d14c9ee874cfc1949a0a9b3f
Smmmhhh <kua0408@gmail.com>
2023-10-19 19:19:18 +0900

    [UPDATE] chat & mypage css & db

--- src/main/java/com/treemarket/tree/service/ChatroomService.java
+++
@@ -1,8 +0,0 @@
-package com.treemarket.tree.service;
-
-import com.treemarket.tree.dto.Chatroom.ChatroomRequest;
-
-public interface ChatroomService {
-
-    void saveChatroom(ChatroomRequest chatroomRequest);
-}
