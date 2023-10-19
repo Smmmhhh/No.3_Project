f77e1c265b1b20080fe796fa8f611606f8870be6
elane <chagaram0610@gmail.com>
2023-10-19 15:47:22 +0900

    UPDATE : 게시글 디테일 진행중

--- src/main/frontend/src/Products/ProductDetail.js
+++ src/main/frontend/src/Products/ProductDetail.js
@@ -3,30 +3,37 @@ import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const { getPostId } = useParams();
-  const [productInfo, setProductInfo] = useState({
-    postId: 0,
-    userNickname: "",
-    title: "",
-    price: "",
-    ctgName: "",
-    details: "",
-    addressName: "",
-    image: [],
-  });
+  const [productInfo, setProductInfo] = useState({});
 
   useEffect(() => {
-    const res = fetch(`/products/detail/${getPostId}`, {
+    fetch(`/products/detail/${getPostId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-    });
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        switch (res.status) {
+          case 200:
+            setProductInfo(res.data);
+            break;
+          case 409:
+            break;
+          default:
+            console.error("Unexpected response status:", res.status);
+        }
+      });
   }, []);
+
+  useEffect(() => {
+    console.log(productInfo);
+  }, [productInfo]);
   return (
     <div className="product-detail">
       <img className="product-img" src="/assets/logo.png" />
       <img className="prodile-img" src="/assets/profile_default.png" />
-      {/* <p className="profile-nickname">{productInfo.userNickname}</p> */}
+      <p className="profile-nickname">{productInfo.userNickname}</p>
     </div>
   );
 };
