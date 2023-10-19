c60e8a5a4ba4ed095539438cdc3d19e52099d7cf
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-10-19 10:18:46 +0900

    [UPDATE] merge

--- src/main/java/com/treemarket/tree/service/ProductPostServiceImpl.java
+++ src/main/java/com/treemarket/tree/service/ProductPostServiceImpl.java
@@ -74,21 +74,6 @@ public class ProductPostServiceImpl implements ProductPostService {
     }
 
     @Override
-    public String joinUrls(List<String> urls) {
-        StringBuilder joinUrl = new StringBuilder();
-        for(int i = 0; i < urls.size(); i++) {
-            joinUrl.append(urls.get(i));
-            if(i+1 < urls.size()) joinUrl.append(",");
-        }
-        return joinUrl.toString();
-    }
-
-    @Override
-    public List<ProductAllBoardResponse> replaceAllBoardResponse(List<ProductPostVO> productPostVOList) {
-        return null;
-    }
-
-    @Override
     public List<String> parseAddress(String joinUrl) {
         String[] parseUrlsArr = joinUrl.split(",");
         List<String> parseUrlsList = new ArrayList<>();
