137fd156294d0e2e8dfe7e6058515a0aba94584f
Smmmhhh <kua0408@gmail.com>
2023-10-20 15:46:20 +0900

    [UPDATE] 채팅업데이트

--- src/main/frontend/src/chat/ChatComponent.js
+++ src/main/frontend/src/chat/ChatComponent.js
@@ -1,74 +1,100 @@
-import React, { useState, useEffect } from "react";
-import { Client } from "@stomp/stompjs"; // 수정된 부분
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const ChatComponent = () => {
-    const [stompClient, setStompClient] = useState(null);
-    const [messages, setMessages] = useState([]);
-    const [messageInput, setMessageInput] = useState("");
-
-    useEffect(() => {
-        const connectWebSocket = () => {
-            const socket = new WebSocket("ws://localhost:8881/ws");
-            const stomp = new Client();
-
-            stomp.webSocketFactory = () => socket;
-
-            stomp.onConnect = () => {
-                stomp.subscribe("/topic/messages", (message) => {
-                    const receivedMessage = JSON.parse(message.body);
-                    setMessages([...messages, receivedMessage]);
-                });
-            };
-
-            stomp.activate();
-            setStompClient(stomp);
+class ChatApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            room_name: '',
+            chatrooms: [],
         };
+    }
 
-        connectWebSocket();
+    componentDidMount() {
+        this.findAllRoom();
+    }
 
-        return () => {
-            if (stompClient) {
-                stompClient.deactivate();
-            }
-        };
-    }, [messages, stompClient]);
-
-    const sendMessage = () => {
-        if (stompClient && messageInput.trim() !== "") {
-            const newMessage = {
-                content: messageInput,
-                sender: "user", // You can replace this with the actual user information
-            };
-
-            stompClient.publish({
-                destination: "/app/chat",
-                body: JSON.stringify(newMessage),
+    findAllRoom = () => {
+        axios.get('/chat/rooms')
+            .then((response) => {
+                this.setState({ chatrooms: response.data });
+            })
+            .catch(() => {
+                alert('채팅방을 불러오는데 실패했습니다.');
             });
-            setMessageInput("");
+    };
+
+    createRoom = () => {
+        const { room_name } = this.state;
+        if (!room_name) {
+            alert('방 제목을 입력해 주세요.');
+            return;
+        }
+
+        const params = new URLSearchParams();
+        params.append('name', room_name);
+
+        axios.post('/chat/room', params)
+            .then((response) => {
+                alert(response.data.roomName + ' 방 개설에 성공했습니다.');
+                this.setState({ room_name: '' });
+                this.findAllRoom();
+            })
+            .catch(() => {
+                alert('채팅방을 개설하는데 실패했습니다.');
+            });
+    };
+
+    enterRoom = (roomId) => {
+        const sender = prompt('닉네임을 입력해 주세요.');
+        if (sender) {
+            localStorage.setItem('wschat.sender', sender);
+            localStorage.setItem('wschat.roomId', roomId);
+            window.location.href = `/chat/room/enter/${roomId}`;
         }
     };
 
-    return (
-        <div>
-            <div>
-                <div>
-                    {messages.map((msg, index) => (
-                        <div key={index}>
-                            <strong>{msg.sender}:</strong> {msg.content}
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div>
-                <input
-                    type="text"
-                    value={messageInput}
-                    onChange={(e) => setMessageInput(e.target.value)}
-                />
-                <button onClick={sendMessage}>Send</button>
-            </div>
-        </div>
-    );
-};
+    render() {
+        const { room_name, chatrooms } = this.state;
 
-export default ChatComponent;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h3>채팅방 리스트</h3>
+                    </div>
+                </div>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <label className="input-group-text">방제목</label>
+                    </div>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={room_name}
+                        onChange={(e) => this.setState({ room_name: e.target.value })}
+                        onKeyUp={(e) => { if (e.key === 'Enter') this.createRoom(); }}
+                    />
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="button" onClick={this.createRoom}>
+                            채팅방 개설
+                        </button>
+                    </div>
+                </div>
+                <ul className="list-group">
+                    {chatrooms.map((item) => (
+                        <li
+                            className="list-group-item list-group-item-action"
+                            key={item.roomId}
+                            onClick={() => this.enterRoom(item.roomId)}
+                        >
+                            {item.roomName}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ChatApp;
