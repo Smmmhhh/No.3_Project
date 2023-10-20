00cc00cbe6e6da3df7e6b032743d0d872d8f7d8f
yoonhyungwoo <guddn13212@naver.com>
2023-10-20 15:48:01 +0900

    FIX : font 수정

--- src/main/frontend/src/MyPage/MyPage.js
+++ src/main/frontend/src/MyPage/MyPage.js
@@ -1,7 +1,7 @@
 import MyFooter from "../MyFooter";
 import MyHeader from "../MyHeader";
 import MyPageForm from "./MyPageForm";
-
+import "./MyPageForm.css";
 const MyPage = () => {
   return (
     <div>
@@ -9,7 +9,19 @@ const MyPage = () => {
         <MyHeader />
         <MyPageForm />
       </div>
-      <MyFooter />
+      <div className="footer">
+        <div className="footer_section">
+          <p>
+            ∙ 사업자명 : 윤나무 ∙ 사업자번호 : 111-11-11111 ∙ 직업정보 제공사업
+            신고번호 : J1122334455667
+          </p>
+          <p>
+            ∙ 전화번호 : 1544-0000 ∙ 주소 : 서울특별시 나무구 나무로 28길 30,
+            1001호 (나무마켓)
+          </p>
+        </div>
+        <img src="/assets/logo.png" />
+      </div>
     </div>
   );
 };
